2ed945c9c8e140195c9ad359e4f3865aaca08096
KartikAmbupe <kartikambupe@gmail.com>
2025-01-02 19:47:16 +0530

    password-gen

--- 05_password-generator/src/App.jsx
+++ 05_password-generator/src/App.jsx
@@ -25,10 +25,45 @@ function App() {
 
   return (
     <>
-      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 bg-gray-800'>
-        <h1 className="text-white text-center ">Password Generator</h1>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+        <h1 className='text-white text-center my-3'>Password generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+            <input
+                type="text"
+                value={password}
+                className="outline-none w-full py-1 px-3"
+                placeholder="Password"
+                readOnly
+                // ref={passwordRef}
+            />
+            <button
+            // onClick={copyPasswordToClipboard}
+            className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+            >Copy</button>
+        </div>
+        <div className='flex test-sm gap-x-2 px-3 mb-3'>
+          <div className='flex items-center gap-x-1'>
+            <input 
+              type="range"
+              min={6}
+              max={50}
+              value={length}
+              className='cursor-pointer'
+              onChange={(e) => {setLength(e.target.value)}}
+            />
+            <label>Length: {length}</label>
+          </div>
+          <div className='flex items-center gap-x-1'>
+            <input 
+              type="checkbox" 
+              defaultChecked={includeNumber}
+              id='numberInput'
+              onChange={() => {setIncludeNumber((prev) => !prev)}}
+            />
+          </div>
+        </div>
       </div>
-        <input type="text" value={password} className='outline-none w-full py-1 px-3 text-black' placeholder='Password' readOnly />
+
     </>
   )
 }
