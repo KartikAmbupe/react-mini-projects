340dc457bbc0ad6942067d9493bfd91b0589adaa
KartikAmbupe <kartikambupe@gmail.com>
2025-01-03 23:14:23 +0530

    password-gen

--- 05_password-generator/src/App.jsx
+++ 05_password-generator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useCallback, useEffect, useRef } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
