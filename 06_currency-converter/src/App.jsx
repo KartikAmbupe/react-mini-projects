0dcd687686d30fa3468926f421772ab5924794df
KartikAmbupe <kartikambupe@gmail.com>
2025-01-05 03:14:05 +0530

    currency-converter

---
+++ 06_currency-converter/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat'
+    style={{backgroundImage: 'url(https://d2908q01vomqb2.cloudfront.net/9e6a55b6b4563e652a23be9d623ca5055c356940/2021/05/11/digital-currency.jpg)'}}>
+      <div className="w-full">
+        <div className=""></div>
+      </div>
+    </div>
+
+  )
+}
+
+export default App
