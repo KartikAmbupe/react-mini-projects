d009fd033a7ceed13d6177ebf32cfed7e24eeaf4
KartikAmbupe <kartikambupe@gmail.com>
2025-01-07 23:21:32 +0530

    custom-hook

---
+++ 06_currency-converter/src/components/InputBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const InputBox = () => {
+  return (
+    <div className='mainBox w-[400px] h-[300px] bg-black'>
+      
+    </div>
+  )
+}
+
+export default InputBox;
