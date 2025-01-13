0eaaeb5393b4ed32218a3b7e631410c140e29b5e
KartikAmbupe <kartikambupe@gmail.com>
2025-01-13 20:34:12 +0530

    shatayu-bg

--- 07_React-router/src/components/Home/Home.jsx
+++ 07_React-router/src/components/Home/Home.jsx
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom'
 import awla from '../../assets/Images/awla-juice.png'
+import { FaShoppingCart } from "react-icons/fa";
 
 
 function Home() {
     return (    
             <div className="mx-auto w-full max-w-7xl">
-                <aside className="relative overflow-hidden text-white rounded-lg sm:mx-16 mx-2 sm:py-3 py-2 ">
-                    <div className="relative z-10 max-w-screen-xl px-4 pb-10 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
+                <aside className="relative overflow-hidden text-white rounded-lg sm:mx-16 mx-2 sm:py-3 py-2">
+                    <div className="relative z-10 max-w-screen-xl px-4 pb-4 pt-4 sm:py-12 mx-auto sm:px-6 lg:px-8">
                         <div className="flex flex-col sm:flex-row items-center justify-between">
                             <div className="sm:w-1/2 flex justify-center sm:justify-start">
                                 <img className="w-full h-full object-contain" src={awla} alt="image1" />
@@ -20,28 +21,12 @@ function Home() {
                                     className="inline-flex text-white items-center px-6 py-3 font-medium bg-orange-700 rounded-lg hover:opacity-75"
                                     to="/"
                                 >
-                                    <svg
-                                        fill="white"
-                                        width="24"
-                                        height="24"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fillRule="evenodd"
-                                        clipRule="evenodd"
-                                    >
-                                        <path d="M1.571 23.664l10.531-10.501 3.712 3.701-12.519 6.941c-.476.264-1.059.26-1.532-.011l-.192-.13zm9.469-11.56l-10.04 10.011v-20.022l10.04 10.011zm6.274-4.137l4.905 2.719c.482.268.781.77.781 1.314s-.299 1.046-.781 1.314l-5.039 2.793-4.015-4.003 4.149-4.137zm-15.854-7.534c.09-.087.191-.163.303-.227.473-.271 1.056-.275 1.532-.011l12.653 7.015-3.846 3.835-10.642-10.612z" />
-                                    </svg>
-                                    &nbsp; Download now
+                                    <FaShoppingCart className='mr-2'/> Order now
                                 </Link>
                             </div>
                         </div>
                     </div>
                 </aside>
-
-                <div className="grid place-items-center sm:mt-20">
-                    <img className="sm:w-96 w-48" src="https://i.ibb.co/2M7rtLk/Remote1.png" alt="image2" />
-                </div>
-
-                <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Lorem Ipsum Yojo</h1>
             </div>
     );
 }
