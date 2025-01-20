df449cf5112d48729c1963283815220a9115cae2
KartikAmbupe <kartikambupe@gmail.com>
2025-01-20 21:27:15 +0530

    context-api

---
+++ 07_context-API/src/context/userContext.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const UserContext = React.createContext(); //creating a context
+//this will create a context object that we can use to provide and consume values
+
+
+
+export default UserContext;
